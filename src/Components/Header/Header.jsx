1e7e75ebb2b13d4642974515f5b12a28258f416c
Mazharul Islam <mazharul3islam0@gmail.com>
2024-11-06 19:46:33 +0600

    push

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -32,7 +32,7 @@ const Header = () => {
                             {links}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-3xl">Book Vibe</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
