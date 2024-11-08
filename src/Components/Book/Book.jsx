28a75f3464a18f55002bc68c3f1be2f0e117fd1b
Mazharul Islam <mazharul3islam0@gmail.com>
2024-11-09 01:12:55 +0600

    push

--- src/Components/Book/Book.jsx
+++ src/Components/Book/Book.jsx
@@ -1,11 +1,20 @@
 
-const Book = ({book}) => {
+const Book = ({ book }) => {
 
-    const {bookName} = book;
+    const { bookName, image } = book;
 
     return (
-        <div>
-            <h2>{bookName}</h2>
+        <div className=" border-2 bg-base-100">
+            <figure>
+                <img src={image} alt="Shoes" />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Books/Books.jsx
+++ src/Components/Books/Books.jsx
@@ -12,9 +12,9 @@ const Books = () => {
 
     return (
         <div>
-            <div className="text-center">
-                <h2 className="text-4xl font-bold">Books : {books.length}</h2>
-                <div>
+            <div>
+                <h2 className="text-4xl text-center w-11/12 font-bold">Books : {books.length}</h2>
+                <div className="grid grid-cols-3 gap-6 w-11/12 mx-auto">
                     {
                         books.map(book=> <Book key={book.id} book={book}></Book>)
                     }
