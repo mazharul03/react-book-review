189c6c5aee181a823a3a2663ec18b14c7ef29146
Mazharul Islam <mazharul3islam0@gmail.com>
2024-11-08 10:36:36 +0600

    push

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -4,10 +4,10 @@ const Banner = () => {
         <div className="bg-gray-200 p-5 rounded-2xl my-10 w-11/12 mx-auto">
             <div className="w-10/12 mx-auto my-12 flex justify-between">
                 <div>
-                    <h2 className="text-5xl my-12 space-y-2 font-bold">Books to freshen up 
+                    <h2 className="text-5xl mt-20 space-y-2 font-bold">Books to freshen up 
                         <br />
                         your bookshelf</h2>
-                    <button className="bg-green-500 text-white py-1 px-3 rounded-md">View The List</button>
+                    <button className="bg-green-500 text-white py-1 mt-16 px-3 rounded-md">View The List</button>
                 </div>
                 <div>
                     <img className="w-80 rounded-lg m-3" src="/src/assets/images/pexels-indraprojectsofficial-28716476.jpg" alt="" />
