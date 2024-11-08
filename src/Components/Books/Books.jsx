bd14b622aeeb1fe79281a4dedf46aea2f407d209
Mazharul Islam <mazharul3islam0@gmail.com>
2024-11-08 12:00:55 +0600

    push

---
+++ src/Components/Books/Books.jsx
@@ -0,0 +1,12 @@
+
+const Books = () => {
+    return (
+        <div>
+            <div className="text-center">
+                <h2 className="text-4xl font-bold">Books</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Books;
