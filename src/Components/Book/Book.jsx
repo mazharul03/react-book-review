16f8d8d1e4c5ef06004ccb6bfe5480d05abbdf9e
Mazharul Islam <mazharul3islam0@gmail.com>
2024-11-09 02:13:03 +0600

    push

--- src/Components/Book/Book.jsx
+++ src/Components/Book/Book.jsx
@@ -4,17 +4,13 @@ const Book = ({ book }) => {
     const { bookName, image } = book;
 
     return (
-        <div className=" border-2 bg-base-100">
-            <figure>
-                <img src={image} alt="Shoes" />
-            </figure>
-            <div className="card-body">
-                <h2 className="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                </div>
-            </div>
+        <div className=" border-2 rounded-lg h-80 w-72">
+
+            <img className="h-52 w-60 mt-4 bg-zinc-100 py-6 px-10 rounded-xl mx-auto " src={image} alt="Shoes" />
+            <h2 className="">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+                
+            
         </div>
     );
 };
