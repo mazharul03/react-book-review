8a1d572bc723066247daf4e4952132162766018c
Mazharul Islam <mazharul3islam0@gmail.com>
2024-11-10 21:03:04 +0600

    push

--- src/Components/Book/Book.jsx
+++ src/Components/Book/Book.jsx
@@ -6,7 +6,10 @@ const Book = ({ book }) => {
     return (
         <div className=" border rounded-xl mx-auto h-96 w-80">
             <img className="h-52 w-72 mt-4 bg-zinc-100 py-6 px-8 rounded-xl mx-auto " src={image} alt="Shoes" />
-            <p className="">{tags}</p>
+            <div>
+                <p className="bg-zinc-100">{tags[0]}</p>
+            </div>
+
             <h2 className="">Shoes!</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
         </div>
