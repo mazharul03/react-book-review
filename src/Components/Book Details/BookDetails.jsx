bb0f56e3338316d94d05119a24711551a72a242b
Mazharul Islam <mazharul3islam0@gmail.com>
2024-11-13 23:00:32 +0600

    push

--- src/Components/Book Details/BookDetails.jsx
+++ src/Components/Book Details/BookDetails.jsx
@@ -4,11 +4,18 @@ const BookDetails = () => {
 
     const books = useLoaderData();
     const {bookId} = useParams();
-    console.log(bookId,books);
+    const book = books.find(book => book.bookId === bookId);
+    console.log(book);
 
     return (
         <div>
-            <h2>BookDetails</h2>
+            <div className="w-10/12 mx-auto flex">
+                <img className="" src={book.image} alt="" />
+                <div>
+                    <h2>{book.bookName}</h2>
+                    <p></p>
+                </div>
+            </div>
         </div>
     );
 };
