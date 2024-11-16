d3901b42a0fd1e63c9802a1106e2cb5155e18d01
Mazharul Islam <mazharul3islam0@gmail.com>
2024-11-16 23:58:18 +0600

    push

--- src/Components/ListedBooks/ListedBooks.jsx
+++ src/Components/ListedBooks/ListedBooks.jsx
@@ -1,21 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getStoredReadBooks } from "../../utility/localstorage";
+import ReadList from "../Read List/ReadList";
 
 
 const ListedBooks = () => {
 
     const  books = useLoaderData();
+
+    const [listedBooks, setListedBooks] = useState([]);
+
     useEffect(() =>{
         const storedBookIds = getStoredReadBooks();
         if(books.length > 0){
-            const booksListed = books.filter(book =>)
+            const booksListed = [];
+            for (const id of storedBookIds){
+                const book = books.find(book => book.id === id);
+                if (book){
+                    booksListed.push(book);
+                }
+            }
+            setListedBooks(booksListed);
         }
     },[])
 
     return (
         <div>
-            <h2>Listed Books</h2>
+            <ReadList></ReadList>
+            <div>
+                {
+                    listedBooks.map(book => <ReadList key={book.id} book={book}></ReadList>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/Components/Read List/ReadList.jsx
@@ -0,0 +1,13 @@
+
+const ReadList = ({book}) => {
+
+    const {bookName} = book;
+
+    return (
+        <div>
+            <h2>{bookName}</h2>
+        </div>
+    );
+};
+
+export default ReadList;
