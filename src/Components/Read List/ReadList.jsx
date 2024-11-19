ee92ad9cfe54cfb7c5db6edf02481a96df112ea4
Mazharul Islam <mazharul3islam0@gmail.com>
2024-11-19 19:30:49 +0600

    push

--- src/Components/Read List/ReadList.jsx
+++ src/Components/Read List/ReadList.jsx
@@ -1,3 +1,4 @@
+import { GrLocation } from "react-icons/gr";
 
 const ReadList = ({ book }) => {
 
@@ -8,10 +9,22 @@ const ReadList = ({ book }) => {
             <div>
                 <img className="w-60 h-64 p-10 rounded-xl bg-gray-200 m-8" src={image} alt="" />
             </div>
-            <div>
+            <div className="space-y-3">
+                <h2 className="text-2xl mt-7 font-bold">{bookName}</h2>
+                <h1> By : {author}</h1>
+                <div className="flex items-center gap-5">
+                    <p className="font-bold text-xl">Tag</p>
+                    <p className="bg-green-50 px-3 rounded-3xl py-2 font-semibold text-green-600">#{book.tags[0]}</p>
+                    <p className="bg-green-50 px-3 rounded-3xl py-2 font-semibold text-green-600">#{book.tags[1]}</p>
+                    <div className="flex items-center gap-2">
+                        <p className="text-xl"><GrLocation /></p>
+                        <p> Year of Publishing : {yearOfPublishing}</p>
+                    </div>
+                </div>
+
+
 
             </div>
-            <h2>{bookName}</h2>
         </div>
     );
 };
