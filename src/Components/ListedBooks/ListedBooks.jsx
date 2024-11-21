79fd5a02e36b85efcd777d9bbeaa391d0c8315a9
Mazharul Islam <mazharul3islam0@gmail.com>
2024-11-21 23:33:51 +0600

    push

--- src/Components/ListedBooks/ListedBooks.jsx
+++ src/Components/ListedBooks/ListedBooks.jsx
@@ -5,7 +5,7 @@ import ReadList from "../Read List/ReadList";
 import WishList from "../Wishlist/WishList";
 
 
-const ListedBooks = () => {
+const ListedBooks = ({readList, wishList}) => {
 
     const books = useLoaderData();
 
@@ -34,26 +34,39 @@ const ListedBooks = () => {
         <div>
             <div className="w-10/12 mx-auto">
                 <h2 className="font-bold text-3xl text-center bg-gray-100 py-7 my-6 rounded-xl">Books</h2>
-                <Link to=''><button> Read Books</button></Link>
-                <Link to={`wishList`}><button> Wishlist Books</button></Link>
-                <div>
-                    {listedBooks.length > 0 ? (
-                        listedBooks.map(book => <ReadList key={book.bookId} book={book} />)
+                <div className="flex items-center -mx-4 overflow-x-auto overflow-y-hidden sm:justify-center flex-nowrap dark:bg-gray-100 dark:text-gray-800">
+                    <a rel="noopener noreferrer" href="#" className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border-b dark:border-gray-600 dark:text-gray-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
+                            <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+                        </svg>
+                        <span>Architecto</span>
+                    </a>
+                    <a rel="noopener noreferrer" href="#" className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border border-b-0 rounded-t-lg dark:border-gray-600 dark:text-gray-900">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
+                            <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
+                            <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
+                        </svg>
+                        <span>Corrupti</span>
+                    </a>
+                </div>
+                {/* <div>
+                    {readList.length > 0 ? (
+                        readList.map(book => <ReadList key={book.bookId} book={book} />)
                     ) : (
                         <p>No books have been marked as read yet.</p>
                     )}
                 </div>
                 <div>
-                    {listedBooks.length > 0 ? (
-                        listedBooks.map(book => <WishList key={book.bookId} book={book} />)
+                    {wishList.length > 0 ? (
+                        wishList.map(book => <WishList key={book.bookId} book={book} />)
                     ) : (
                         <p>No books have been marked as read yet.</p>
                     )}
-                </div>
+                </div> */}
             </div>
         </div>
     );
-    
+
 
 };
 
